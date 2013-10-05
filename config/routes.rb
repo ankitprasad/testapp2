496e61638e0b34e3e069f950e863e91f54ec9139
ankitprasad <mail@ankitprasad.net>
2013-10-05 11:47:48 -0400

    Creating Splash Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Testapp2::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+ root :to => 'staticpages#index'
+
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -55,4 +58,5 @@ Testapp2::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
